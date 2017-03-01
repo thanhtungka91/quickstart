74a7285d577568dbfee62272a5230d37274db4a1
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-03-02 01:20:25 +0900

    use template url

--- src/app/hero-detail.component.js
+++ src/app/hero-detail.component.js
@@ -20,7 +20,7 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent = __decorate([
         core_1.Component({
             selector: 'my-hero-detail',
-            template: "\n    <div *ngIf=\"hero\">\n      <h2>{{hero.name}} details!</h2>\n      <div><label>id: </label>{{hero.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n  "
+            templateUrl: 'app/hero_detail.html'
         }), 
         __metadata('design:paramtypes', [])
     ], HeroDetailComponent);
